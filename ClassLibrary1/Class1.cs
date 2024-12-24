7ba2ee075310fce5c809d1880b608ec6a09ac2ad
Forgisih <ivankalmykov836@gmail.com>
2024-12-24 09:51:59 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Flower
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Flower (string T, string D)
+        { 
+            Title = T; Description = D;
+        }
+        public void Show () 
+        {
+            Console.WriteLine($"Цветочек: Название: {Title}, Описание: {Description}");
+        }
+    }
+    public class Garden
+    {
+        public string Adress { get; set; }
+        public string Owner { get; set; }
+        public List<Flower> flowers = new List<Flower>();
+
+        public void ShowAll() 
+        {
+            Console.WriteLine($"Сад: Адрес: {Adress}, Владелец: {Owner}, Цветы: ");
+            foreach (var flower in flowers)
+            {
+                flower.Show();
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flower flower = new Flower("Роза", "Красные лепестки");
+            Flower flower1 = new Flower("Ромашка", "Белые лепестки");
+            Flower flower2 = new Flower("Рододендрон", "Светло-фиолетовые лепестки");
+
+            Garden garden = new Garden();
+            garden.Adress = "г Калыма, ул Севастополь, д 9, кв 71";
+            garden.Owner = "Геннадий Альбертович";
+
+            garden.flowers.Add(flower);
+            garden.flowers.Add(flower1);
+            garden.flowers.Add(flower2);
+
+            garden.ShowAll();
+            Console.ReadLine();
+        }
+    }
+}
